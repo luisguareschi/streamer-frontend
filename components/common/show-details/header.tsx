e639716cba569e221f65584e34541fef567b08f7
luisguareschi <luisguareschi@hotmail.com>
2025-03-06 11:15:53 +0100

    Added share button

--- components/common/show-details/header.tsx
+++ components/common/show-details/header.tsx
@@ -1,9 +1,16 @@
 import { ChevronLeft, Plus, Share2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { toast } from "react-hot-toast";
 
 export const Header = () => {
   const router = useRouter();
+
+  const handleShare = () => {
+    navigator.clipboard.writeText(window.location.href);
+    toast.success("Link copied to clipboard");
+  };
+
   return (
     <div className="flex justify-between items-start sm:fixed sm:top-0 sm:left-0 sm:w-full sm:p-4">
       <Button variant="glass" size="icon" onClick={() => router.back()}>
@@ -13,7 +20,7 @@ export const Header = () => {
         <Button variant="glass" size="icon">
           <Plus className="size-6 min-w-6 min-h-6 drop-shadow-lg" />
         </Button>
-        <Button variant="glass" size="icon">
+        <Button variant="glass" size="icon" onClick={handleShare}>
           <Share2 className="size-6 min-w-6 min-h-6 drop-shadow-lg" />
         </Button>
       </div>
